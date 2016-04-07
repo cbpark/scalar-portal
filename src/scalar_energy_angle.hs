a2f9f3097e690489b14460e98e750a0f0599e9fe
cbpark <cbpark@gmail.com>
2016-04-07 18:20:01 +0900

    Update scalar_energy_angle.hs

--- src/scalar_energy_angle.hs
+++ src/scalar_energy_angle.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import qualified HEP.Data.LHEF.PipesUtil    as U
+
+import           Codec.Compression.GZip     (decompress)
+import qualified Data.ByteString.Lazy.Char8 as L
+import           Data.List                  (intercalate)
+import           Pipes
+import qualified Pipes.Prelude              as P
+import           System.Environment         (getArgs)
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+    let header = "# " ++ intercalate ", " [ "energy", "cosTh"]
+    putStrLn header
+
+    infile <- fmap head getArgs
+    evStr <- fmap decompress (L.readFile infile)
+    runEffect $ (U.eventEntryFromBS . L.toStrict) evStr
+        >-> U.finalStates
+        >-> P.print
