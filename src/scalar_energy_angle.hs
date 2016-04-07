f89e295c2fee8ae1dd93ea3abe2f2320d01ac569
cbpark <cbpark@gmail.com>
2016-04-07 23:34:45 +0900

    Add scalar_energy_hist.hs, update scalar_energy_angle.hs

--- src/scalar_energy_angle.hs
+++ src/scalar_energy_angle.hs
@@ -1,8 +1,10 @@
 module Main where
 
+import           HEP.Data.LHEF
 import qualified HEP.Data.LHEF.PipesUtil    as U
 
 import           Codec.Compression.GZip     (decompress)
+import           Control.Monad              (forever)
 import qualified Data.ByteString.Lazy.Char8 as L
 import           Data.List                  (intercalate)
 import           Pipes
@@ -11,11 +13,26 @@ import           System.Environment         (getArgs)
 
 main :: IO ()
 main = do
-    let header = "# " ++ intercalate ", " [ "energy", "cosTh"]
+    let header = "# " ++ intercalate ", " [ "energy", "cosTheta"]
     putStrLn header
 
     infile <- fmap head getArgs
     evStr <- fmap decompress (L.readFile infile)
     runEffect $ (U.eventEntryFromBS . L.toStrict) evStr
-        >-> U.finalStates
-        >-> P.print
+        >-> U.finalStates >-> P.map (filter isScalar)
+        >-> variables
+        >-> P.mapM_ (mapM_ print)
+      where
+        isScalar = (== 35) . idOf
+
+newtype Result = Result { getResult :: [(String, Double)]}
+
+instance Show Result where
+    show = intercalate ", " . map (show . snd) . getResult
+
+variables :: Monad m => Pipe [Particle] [Result] m ()
+variables = forever $ do
+    ps <- await
+    let calcVar = (,) <$> energyOf <*> cosThetaBeam
+        results = map ((\(x, y) -> [x, y]) . calcVar) ps
+    yield $ map (Result . zip ["energy", "cosTheta"]) results
---
+++ src/scalar_energy_hist.hs
@@ -0,0 +1,50 @@
+module Main where
+
+import           HEP.Analysis.Histogram1D
+
+import           Control.Monad.Trans.State.Strict
+import           Data.Attoparsec.ByteString       (skipWhile)
+import           Data.Attoparsec.ByteString.Char8 hiding (skipWhile)
+import           Data.ByteString.Char8            (ByteString)
+import           Data.List                        (intercalate, transpose)
+import           Pipes
+import qualified Pipes.Attoparsec                 as PA
+import           Pipes.ByteString                 (fromHandle)
+import qualified Pipes.Prelude                    as P
+import           System.Environment               (getArgs)
+import           System.IO                        (Handle, IOMode (..),
+                                                   withFile)
+
+main :: IO ()
+main = do
+    putStrLn $ "# " ++ intercalate ", " ["bins", "energy"]
+
+    infile <- fmap head getArgs
+    histograms infile
+      where
+        histograms infile = do
+            h <- withFile infile ReadMode (getEnergyHist 110 0.0 220.0)
+            (mapM_ putStrLn . showHist) h
+
+getEnergyHist :: MonadIO m => Int -> Double -> Double -> Handle -> m (Hist1D Double)
+getEnergyHist nbin lo hi hin = P.fold mappend mempty id hist
+  where hist = (getEnergy . fromHandle) hin >-> P.map (histogram1 nbin lo hi)
+
+getEnergy :: Monad m => Producer ByteString m () -> Producer Double m ()
+getEnergy s = do
+    (r, s') <- lift $ runStateT (PA.parse energy) s
+    case r of Just (Right ev) -> yield ev >> getEnergy s'
+              _               -> return ()
+      where
+        energy :: Parser Double
+        energy = do
+            skipSpace
+            _ <- many' $ char '#' >> skipWhile (not . isEndOfLine) >> endOfLine
+            e <- double
+            char ',' >> skipSpace
+            _ <- double <* skipSpace
+            return e
+
+showHist :: Hist1D Double -> [String]
+showHist h = let (bs, con) = ((,) <$> bins <*> contents) h
+             in map (intercalate ", " . map show) (transpose [bs, con])
